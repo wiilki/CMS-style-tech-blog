566da7376ec517a3d369791b078a2dc0dec2483f
Will <willkim0608@gmail.com>
2023-01-27 12:18:02 -0800

    Add more GET routes. Edit homepage. Import basic stylesheet

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('homepage', { 
-        title: 'Homepage', 
-        message: 'Welcome to the homepage' 
+router.get('/', async (req, res) => {
+  try {
+    // Get all posts and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
     });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/login', (req, res) => {
