e5f7960d72e1b747004d99c0ed8101f976b0f6c2
Will <willkim0608@gmail.com>
2023-01-27 13:22:45 -0800

    Add logout and dashboard logic

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,29 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try {
-    // Get all posts and JOIN with user data
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
+router.get('/', (req, res) => {
     res.render('homepage', { 
-      posts, 
-      logged_in: req.session.logged_in 
+        title: 'Homepage', 
+        message: 'Welcome to the homepage' 
     });
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
 router.get('/login', (req, res) => {
