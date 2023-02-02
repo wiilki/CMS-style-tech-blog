be7d5dac98a9e9f3e6c8acc004d75c663acb6f2c
Will <willkim0608@gmail.com>
2023-02-01 19:08:36 -0800

    Add GET route for comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,6 +2,15 @@ const router = require("express").Router();
 const { Comment } = require("../../models/");
 const withAuth = require("../../utils/auth");
 
+router.get('/', (req, res) => {
+  Comment.findAll({})
+    .then(newComment => res.json(newComment))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.post("/", withAuth, (req, res) => {
   Comment.create({ ...req.body, userId: req.session.userId })
     .then(newComment => {
