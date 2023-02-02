a3f437d6053524b0bac89047c6c129e5fe09ec7c
Will <willkim0608@gmail.com>
2023-02-01 16:57:03 -0800

    Error with display. Edit dashboardRoutes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,27 +1,37 @@
-const router = require("express").Router();
-const { Post } = require("../models");
-const withAuth = require("../utils/auth");
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get("/", withAuth, (req, res) => {
-  Post.findAll({
-    where: { user_id: req.session.user_id }
-  })
-    .then(allPostData => {
-      const posts = allPostData.map((post) => post.get({ plain: true }));
-
-      res.render("displayPost", {
-        posts
-      });
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+      where: {       user_id: req.session.user_id    },
+      include: [
+        {
+          model: Comment,
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
     })
-    .catch(err => {
-      console.log(err);
-      res.redirect("login");
-    });
-});
-
-router.get("/post", withAuth, (req, res) => {
-  res.render("newPost", {
+      .then(dbPostData => {
+        // serialize data before passing to template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   });
-});
+
+  
+
 
 module.exports = router;
