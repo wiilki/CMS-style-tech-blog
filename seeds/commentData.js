9ee0485b06fc62f90cf9d37184e690b099891084
Will <willkim0608@gmail.com>
2023-02-01 20:19:04 -0800

    Edit seed data

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -3,13 +3,19 @@ const { Comment } = require('../models');
 const commentData =
   [
     {
-      body: "That's awesome!"
+      body: "That's awesome!",
+      user_id: 1,
+      post_id: 3
     },
     {
-      body: "Interesting post"
+      body: "Interesting post",
+      user_id: 2,
+      post_id: 2
     },
     {
-      body: "Cool!"
+      body: "Cool!",
+      user_id: 3,
+      post_id: 1
     }
   ]
 
